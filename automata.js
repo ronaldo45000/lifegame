8864fe50e56cf45bf7379c21491d9d6759e4420d
onme7 <Onme7052@gmail.com>
2024-04-05 10:48:18 -0700

    done

--- automata.js
+++ automata.js
@@ -3,229 +3,101 @@ class Automata {
         Object.assign(this, { game });
 
         this.automata = [];
-        this.height = 100;
-        this.width = 200;
+        this.height = 51;
+        this.width = 118;
 
         this.tickCount = 0;
         this.ticks = 0;
 
-        this.speed = parseInt(document.getElementById("speed").value, 10);
 
-
-        for (let col = 0; col < this.width; col++) {
+            //load empty grid for cells
+        for (let i = 0; i < this.width*2; i++) {
             this.automata.push([]);
-            for (let row = 0; row < this.height; row++) {
-                this.automata[col][row] = 0;
-            }
-        }
-        this.loadRandomAutomata();
-    };
-
-    loadRandomAutomata() {
-        for (let col = 0; col < this.width; col++) {
-            for (let row = 0; row < this.height; row++) {
-                this.automata[col][row] = randomInt(2);
-            }
-        }
-    };
-
-    addBlock(col, row) {
-        this.automata[col][row] = 1;
-        this.automata[col + 1][row] = 1;
-        this.automata[col][row + 1] = 1;
-        this.automata[col + 1][row + 1] = 1;
-    };
-
-    addHive(col, row, vert) {
-        if (vert) {
-            this.automata[col][row] = 1;
-            this.automata[col + 1][row + 1] = 1;
-            this.automata[col - 1][row + 1] = 1;
-            this.automata[col + 1][row + 2] = 1;
-            this.automata[col - 1][row + 2] = 1;
-            this.automata[col][row + 3] = 1;
-        } else {
-            this.automata[col][row] = 1;
-            this.automata[col + 1][row + 1] = 1;
-            this.automata[col + 1][row - 1] = 1;
-            this.automata[col + 2][row + 1] = 1;
-            this.automata[col + 2][row - 1] = 1;
-            this.automata[col + 3][row] = 1;
-        }
-    };
-
-    addLoaf(col, row) {
-        this.automata[col][row] = 1;
-        this.automata[col + 1][row + 1] = 1;
-        this.automata[col + 1][row - 1] = 1;
-        this.automata[col + 2][row + 2] = 1;
-        this.automata[col + 2][row - 1] = 1;
-        this.automata[col + 3][row] = 1;
-        this.automata[col + 3][row + 1] = 1;
-    };
-
-    addBoat(col, row) {
-        this.automata[col][row] = 1;
-        this.automata[col][row + 1] = 1;
-        this.automata[col + 1][row] = 1;
-        this.automata[col + 1][row + 2] = 1;
-        this.automata[col + 2][row + 1] = 1;
-    };
-
-
-    addTub(col, row) {
-        this.automata[col][row] = 1;
-        this.automata[col + 1][row + 1] = 1;
-        this.automata[col + 1][row - 1] = 1;
-        this.automata[col + 2][row] = 1;
-    };
-
-    addBlinker(col, row, vert) {
-        if (vert) {
-            this.automata[col][row] = 1;
-            this.automata[col][row + 1] = 1;
-            this.automata[col][row + 2] = 1;
-        } else {
-            this.automata[col][row] = 1;
-            this.automata[col + 1][row] = 1;
-            this.automata[col + 2][row] = 1;
-        }
-    };
-
-    addToad(col, row) {
-        this.addBlinker(col, row);
-        this.addBlinker(col + 1, row + 1);
-    };
-
-    addBeacon(col, row) {
-        this.addBlock(col, row);
-        this.addBlock(col + 2, row + 2);
-    };
-
-    addPulsar(col, row) {
-        this.addBlinker(col, row, true);
-        this.addBlinker(col + 5, row, true);
-        this.addBlinker(col + 7, row, true);
-        this.addBlinker(col + 12, row, true);
-        this.addBlinker(col, row + 6, true);
-        this.addBlinker(col + 5, row + 6, true);
-        this.addBlinker(col + 7, row + 6, true);
-        this.addBlinker(col + 12, row + 6, true);
-        this.addBlinker(col + 2, row - 2, false);
-        this.addBlinker(col + 2, row + 3, false);
-        this.addBlinker(col + 2, row + 5, false);
-        this.addBlinker(col + 2, row + 10, false);
-        this.addBlinker(col + 8, row - 2, false);
-        this.addBlinker(col + 8, row + 3, false);
-        this.addBlinker(col + 8, row + 5, false);
-        this.addBlinker(col + 8, row + 10, false);
-    };
-
-    addPenta(col, row) {
-        this.automata[col][row] = 1;
-        this.addBlinker(col - 1, row + 1);
-        this.addBlinker(col - 2, row + 2);
-        this.addBlinker(col, row + 2);
-        this.addBlinker(col - 2, row + 9);
-        this.addBlinker(col, row + 9);
-        this.addBlinker(col - 1, row + 10);
-        this.automata[col][row + 11] = 1;
-    };
-
-    addGosper(col, row) {
-        this.addBlock(col, row);
-        this.addBlinker(col + 10, row, true);
-        this.automata[col + 11][row - 1] = 1;
-        this.automata[col + 11][row + 3] = 1;
-        this.automata[col + 12][row - 2] = 1;
-        this.automata[col + 12][row + 4] = 1;
-        this.automata[col + 13][row - 2] = 1;
-        this.automata[col + 13][row + 4] = 1;
-        this.automata[col + 14][row + 1] = 1;
-        this.automata[col + 15][row - 1] = 1;
-        this.automata[col + 15][row + 3] = 1;
-        this.addBlinker(col + 16, row, true);
-        this.automata[col + 17][row + 1] = 1;
-        this.addBlinker(col + 20, row - 2, true);
-        this.addBlinker(col + 21, row - 2, true);
-        this.automata[col + 22][row - 3] = 1;
-        this.automata[col + 22][row + 1] = 1;
-        this.automata[col + 24][row - 3] = 1;
-        this.automata[col + 24][row + 1] = 1;
-        this.automata[col + 24][row - 4] = 1;
-        this.automata[col + 24][row + 2] = 1;
-        this.addBlock(col + 34, row - 2);
-    };
-    
-    loadAutomata() {
-        for (let col = 0; col < this.width; col++) {
-            for (let row = 0; row < this.height; row++) {
-                this.automata[col][row] = 0;
+            for (let j = 0; j < this.height; j++) {
+                this.automata[i][j] = 0;
             }
         }
 
-        this.addBlock(10, 10);
-        this.addHive(14, 11, false);
-        this.addHive(21, 10, true);
-        this.addTub(10, 15);
-        this.addLoaf(15, 16);
-        this.addBoat(21, 16);
-
-        this.addBlinker(30, 11);
-        this.addToad(35, 11);
-        this.addBeacon(41, 10);
-        this.addPulsar(48, 11);
-        this.addPenta(68, 9);
-
-        this.addGosper(10, 50);
+        this.loadRandomCells();
     };
 
-    count(col, row) {
-        let count = 0;
-        for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-                if ((i || j) && this.automata[col + i] && this.automata[col + i][row + j]) count++;
+    loadRandomCells() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                this.automata[i][j] =  Math.floor(Math.random() * 2);
             }
         }
-        return count;
+    };
+
+  
+
+    countMyNeightCells(col, row) {
+        let cell = 0;
+      
+        for (let i = 0; i <= 2; i++) {
+            for (let j = 0; j <= 2; j++) {
+                if (i !== 1 || j !== 1) {
+                    const neighborCol = col + i - 1;
+                    const neighborRow = row + j - 1;
+                    if (neighborCol >= 0 && neighborCol < this.width && neighborRow >= 0 && neighborRow < this.height && this.automata[neighborCol][neighborRow]) {
+                        cell++;
+                    }
+                }
+            }
+        }
+        return cell;
     };
 
     update() {
-        this.speed = parseInt(document.getElementById("speed").value, 10);
+        this.speed = parseInt(document.getElementById("slider").value, 10);
+        this.tickCount++;
+        if (this.tickCount >= this.speed && this.speed != 30) {
+         
+            document.getElementById('ticks').innerHTML = "Ticks: "+ this.ticks;
 
-        if (this.tickCount++ >= this.speed && this.speed != 120) {
+            let arr = [];
+            for (let col = 0; col < this.width; col++) {
+                arr.push([]);
+                for (let row = 0; row < this.height; row++) {
+                    arr[col].push(0);
+                }
+            }
+
+            for (let col = 0; col < this.width; col++) {
+                for (let row = 0; row < this.height; row++) {
+                    //cells condition live and die
+                    if (this.automata[col][row] && (this.countMyNeightCells(col, row) === 2 || this.countMyNeightCells(col, row) === 3)) {
+                        arr[col][row] = 1;
+                    }
+                    if (!this.automata[col][row] && this.countMyNeightCells(col, row) === 3) {
+                        arr[col][row] = 1;
+                    }
+                    if (this.automata[col][row] &&  (this.countMyNeightCells(col, row) > 3 || this.countMyNeightCells(col,row)<2)){
+                        arr[col][row] = 0;
+                    
+            }
+        }}
+            this.automata = [...arr];    
             this.tickCount = 0;
             this.ticks++;
-            document.getElementById('ticks').innerHTML = "Ticks: " + this.ticks;
-
-            let next = [];
-            for (let col = 0; col < this.width; col++) {
-                next.push([]);
-                for (let row = 0; row < this.height; row++) {
-                    next[col].push(0);
-                }
-            }
-
-            for (let col = 0; col < this.width; col++) {
-                for (let row = 0; row < this.height; row++) {
-                    if (this.automata[col][row] && (this.count(col, row) === 2 || this.count(col, row) === 3)) next[col][row] = 1;
-                    if (!this.automata[col][row] && this.count(col, row) === 3) next[col][row] = 1;
-                }
-            }
-            this.automata = next;
         }
     };
 
     draw(ctx) {
-        let size = 8;
+        let size = 20;
         let gap = 1;
-        ctx.fillStyle = "Black";
+ // Draw the image onto the canvas
+ let image = document.getElementById('image');
+
         for (let col = 0; col < this.width; col++) {
             for (let row = 0; row < this.height; row++) {
                 let cell = this.automata[col][row];
-                if (cell) ctx.fillRect(col * size + gap, row * size + gap, size - 2 * gap, size - 2 * gap);
+                if (cell) {
+                    ctx.drawImage(image,  gap+ col * size, gap+ row * size , size - 5 * gap, size - 5 * gap);
+                }
             }
         }
+
     };
 
 };
